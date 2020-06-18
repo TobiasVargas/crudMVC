697783719b67438f1f17961c20bcaefee52f4222
Tobias de Vargas <42094615+TobiasVargas@users.noreply.github.com>
2020-06-18 19:40:27 -0300

    Inicio do CRUD

---
+++ CrudMVC/Data/CrudMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CrudMVC.Models;
+
+namespace CrudMVC.Data
+{
+    public class CrudMVCContext : DbContext
+    {
+        public CrudMVCContext (DbContextOptions<CrudMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrudMVC.Models.Product> Product { get; set; }
+    }
+}
